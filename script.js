c08ee20fc1db2b3a4f8b6fe0e35046ebdf65b800
ryanvandrunen <ryanvandrunen@oscvi.com>
2024-01-06 13:39:59 -0500

    dropdown bullets

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        draggable: true
+    })
+})
+
+function toggleDropdown(dropdownId, elementId) {
+    var dropdownLists = document.querySelectorAll('.dropdown-list');
+    var mainElements = document.querySelectorAll('.mainElement');
+    dropdownLists.forEach(function(list) {
+        list.classList.add('hidden');
+    })
+    mainElements.forEach(function(list) {
+        list.classList.remove('down');
+        list.classList.add('up');
+    })
+
+    var dropdownList = document.getElementById(dropdownId);
+    var dropdownElement = document.getElementById(elementId);
+    dropdownList.classList.toggle('hidden');
+    dropdownElement.classList.toggle('down');
+    dropdownElement.classList.toggle('up');
+  }
